df1d249404a0ca815ce51973a6ee78969dc4bbd9
Godtom17 <158202357+Godtom17@users.noreply.github.com>
2024-06-12 14:02:56 +0100

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-59b04d20-48c1-46d9-88e0-dadb11ef6243/"
 
 OUTPUT_DIR="logs"
 
